902a8232892683f1251000b7b85c08c7e520d26a
EjLaquiorez <ejlqrz@gmail.com>
2026-08-31 20:22:58 +0800

    exercises: variables lesson01

---
+++ exercises/phase-01-java-foundations/01-variables/Lesson01Exercise.java
@@ -0,0 +1,13 @@
+public class Lesson01Exercise {
+    public static void main(String[] args) {
+
+        int score = 50;
+        System.out.println(score);
+
+        score = 75;
+        System.out.println(score);
+
+        score = 100;
+        System.out.println(score);
+    }
+}
