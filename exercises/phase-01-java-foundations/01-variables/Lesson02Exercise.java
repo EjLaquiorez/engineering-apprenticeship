2f3c13a3beea1966e60a57b4c92ee6c0cd60bcc8
EjLaquiorez <ejlqrz@gmail.com>
2026-09-01 08:34:00 +0800

    exercises: variables lesson02

---
+++ exercises/phase-01-java-foundations/01-variables/Lesson02Exercise.java
@@ -0,0 +1,18 @@
+public class Lesson02Exercise {
+
+    public static void main(String[] args) {
+
+        int age = 25;
+        int licenseNumber = 12345;
+
+        if (age >= 18) {
+
+
+            System.out.println(age);
+            System.out.println(licenseNumber);
+        }
+
+        System.out.println(age);
+        System.out.println(licenseNumber);
+    }
+}
