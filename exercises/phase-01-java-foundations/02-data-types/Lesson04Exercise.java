b92967bd084eef430868d0a8a4f1e028e815c2ff
EjLaquiorez <ejlqrz@gmail.com>
2026-09-01 14:19:11 +0800

    exercises: data types lesson04

---
+++ exercises/phase-01-java-foundations/02-data-types/Lesson04Exercise.java
@@ -0,0 +1,19 @@
+public class Lesson04Exercise{
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        int Addition = a + b;
+        int Subtraction = a - b;
+        int Multiplication = a * b;
+        int Division = a / b;
+        int Remainder = a % b;
+
+        System.out.println("Addition: " + Addition);
+        System.out.println("Subtraction: " + Subtraction);
+        System.out.println("Multiplication: " + Multiplication);
+        System.out.println("Division: " + Division);
+        System.out.println("Remainder: " + Remainder);
+    }
+}
