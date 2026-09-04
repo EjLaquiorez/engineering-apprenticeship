90cf5fd796a8848adb5238d67b0a5283b32d6f64
EjLaquiorez <ejlqrz@gmail.com>
2026-09-04 22:40:44 +0800

    exercise(04-methods): add Lesson 17 exercise

---
+++ exercises/phase-01-java-foundations/04-methods/Lesson17Exercise.java
@@ -0,0 +1,10 @@
+public class Lesson17Exercise{
+    public static void main(String[] args) {
+        displayMenu();
+    }
+
+    static void displayMenu(){
+
+        System.out.println("Welcome to the program.");
+    }
+}
