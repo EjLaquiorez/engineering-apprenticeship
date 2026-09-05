93b38d7d6e1fef90cad23e69c3094958c123e44d
EjLaquiorez <ejlqrz@gmail.com>
2026-09-05 16:46:27 +0800

    exercise(04-methods): add Lesson 18 exercise

---
+++ exercises/phase-01-java-foundations/04-methods/Lesson18Exercise.java
@@ -0,0 +1,22 @@
+public class Lesson18Exercise {
+
+    static void displayTitle() {
+        System.out.println("====================");
+        System.out.println("JAVA TRAINING APP");
+        System.out.println("====================");
+    }
+
+    static void displayUser() {
+        System.out.println("User: Maya");
+    }
+
+    static void displayMessage() {
+        System.out.println("Welcome to your coding journey!");
+    }
+
+    public static void main(String[] args) {
+        displayTitle();
+        displayUser();
+        displayMessage();
+    }
+}
