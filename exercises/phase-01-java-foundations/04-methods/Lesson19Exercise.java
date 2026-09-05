b35fb6301930a3cbf76ca58dc2d0e6ed9b1f0f4a
EjLaquiorez <ejlqrz@gmail.com>
2026-09-05 22:39:26 +0800

    exercise(04-methods): add Lesson 19 exercise

---
+++ exercises/phase-01-java-foundations/04-methods/Lesson19Exercise.java
@@ -0,0 +1,16 @@
+public class Lesson19Exercise{
+    public static void main(String[] args) {
+        displayName("Justine");
+        displayAge(27);
+        displayGrade('A');
+    }
+    static void displayName(String name){
+        System.out.println(name);
+    }
+    static void displayAge(int age){
+        System.out.println(age);
+    }
+    static void displayGrade(char grade){
+        System.out.println(grade);
+    }
+}
