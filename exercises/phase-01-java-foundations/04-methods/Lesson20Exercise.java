9c09452f23076e4d7edded74034c8ae2c2579281
EjLaquiorez <ejlqrz@gmail.com>
2026-09-06 16:24:40 +0800

    exercise(04-methods): add Lesson 20 exercise

--- exercises/phase-01-java-foundations/04-methods/Lesson20Exercise.java
+++ exercises/phase-01-java-foundations/04-methods/Lesson20Exercise.java
@@ -1,8 +1,12 @@
 public class Lesson20Exercise{
     public static void main(String[] args) {
-        System.out.println("Total Price: " + calculateTotal(1499.99, 7));
+        double subtotal = calculateSubTotal(1499.99, 7);
+        System.out.println("Subtotal: " + subtotal);
+
+        double subtotal2 = calculateSubTotal(499.99, 8);
+        System.out.println("Subtotal: " + subtotal2);
     }
-    static double calculateTotal(double price, int quantity){
+    static double calculateSubTotal(double price, int quantity){
         return price * quantity;
     }
 
