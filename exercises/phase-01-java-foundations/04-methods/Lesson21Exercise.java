82512ffe16d817359661a66050b5e41928c681fc
EjLaquiorez <ejlqrz@gmail.com>
2026-09-06 16:56:17 +0800

    exercise(04-methods): add Lesson 21 exercise

---
+++ exercises/phase-01-java-foundations/04-methods/Lesson21Exercise.java
@@ -0,0 +1,11 @@
+public class Lesson21Exercise{
+    public static void main(String[] args) {
+        displayStudent("Justine",27);
+    }
+
+    static void displayStudent(String name, int age) {
+    
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
