78686a208b2e4f20436f1575279dbe7cef439c40
EjLaquiorez <ejlqrz@gmail.com>
2026-09-06 12:45:52 +0800

    exercise(04-methods): add Lesson 19 exercise

--- exercises/phase-01-java-foundations/04-methods/Lesson19Exercise.java
+++ exercises/phase-01-java-foundations/04-methods/Lesson19Exercise.java
@@ -1,16 +1,11 @@
 public class Lesson19Exercise{
     public static void main(String[] args) {
-        displayName("Justine");
-        displayAge(27);
-        displayGrade('A');
+        displayStudent("Justine", 27, 'S');
     }
-    static void displayName(String name){
-        System.out.println(name);
-    }
-    static void displayAge(int age){
-        System.out.println(age);
-    }
-    static void displayGrade(char grade){
-        System.out.println(grade);
+    static void displayStudent(String name, int age, char grade) {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Grade: " + grade);
+    // print the student's information
     }
 }
