655d51326d062b39596244b38f7257753f959a8a
EjLaquiorez <ejlqrz@gmail.com>
2026-09-13 16:30:33 +0800

    projects: miniExercise02 Arrays

--- projects/phase-01-java-foundations/expense-tracker/miniExercise02.java
+++ projects/phase-01-java-foundations/expense-tracker/miniExercise02.java
@@ -1,5 +1,18 @@
 public class miniExercise02 {
     public static void main(String[] args) {
-        
+        String[] description = new String[4];
+        double[] amount = new double[4];
+
+        description[0] = "Lunch";
+        amount[0] = 150;
+
+        description[1] = "Jeepney";
+        amount[1] = 30;
+
+        description[2] = "Coffee";
+        amount[2] = 80;
+
+        description[3] = "Groceries";
+        amount[3] = 500;
     }
 }
