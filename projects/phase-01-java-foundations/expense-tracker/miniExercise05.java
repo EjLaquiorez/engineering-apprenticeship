41759f27a1eb8d7297dc60fdffa5983444409b1c
EjLaquiorez <ejlqrz@gmail.com>
2026-09-15 11:38:10 +0800

    projects: miniExercise05 - Total Expenses

--- projects/phase-01-java-foundations/expense-tracker/miniExercise05.java
+++ projects/phase-01-java-foundations/expense-tracker/miniExercise05.java
@@ -3,6 +3,7 @@ public class miniExercise05 {
     public static void main(String[] args) {
         String[] descriptions = new String[3];
         double[] amounts = new double[3];
+        int expenseCount = 0;
 
         try (Scanner scanner = new Scanner(System.in)) {
             String option;
@@ -12,7 +13,7 @@ public class miniExercise05 {
                 System.out.println("=== Expense Tracker ===");
                 System.out.println("1. Add Expense");
                 System.out.println("2. View Expense");
-                System.out.println("3. Exit");
+                System.out.println("3. View Total");
 
                 System.out.print("Choose an option: ");
                 option = scanner.nextLine().trim();
@@ -22,7 +23,42 @@ public class miniExercise05 {
                     System.out.println("Invalid operation.\n");
                     continue;
                 }
+
+                switch (option) {
+                    case "1":
+                        if (expenseCount == descriptions.length) {
+                            System.out.println("Expense list is full.\n");
+                            continue;
+                        }
+
+                        System.out.print("Enter description: ");
+                        descriptions[expenseCount] = scanner.nextLine();
+
+                        System.out.print("Enter amount: ");
+                        amounts[expenseCount] = Double.parseDouble(scanner.nextLine());
+
+                        expenseCount++;
+                        break;
+
+                    case "2":
+                        for(int i = 0; i < expenseCount; i++){
+                            System.out.println((i + 1) + ". " + descriptions[i] + " - " + amounts[i]);
+                        }
+
+
+                        break;
+
+                    case "3":
+                        double totalExpenses = 0;
+                        for(int i = 0; i < expenseCount; i++){
+                            totalExpenses+= amounts[i];
+                        }
+                        System.out.println("Total Expenses: " + totalExpenses);
+                        break;
+                }
             } while (!option.equals("3"));
         }
+        System.out.println("Goodbye!");
     }
+    
 }
