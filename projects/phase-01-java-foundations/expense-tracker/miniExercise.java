c991a7e65e83a374725c5b406d1e5b77555465c0
EjLaquiorez <ejlqrz@gmail.com>
2026-09-16 18:58:55 +0800

    projects: miniExercise -validation

--- projects/phase-01-java-foundations/expense-tracker/miniExercise.java
+++ projects/phase-01-java-foundations/expense-tracker/miniExercise.java
@@ -5,12 +5,18 @@ public class miniExercise {
         double[] amounts = new double[3];
         int expenseCount = 0;
 
-        try(Scanner scanner = new Scanner(System.in)){
-            
-        } catch (Exception e) {
-            // TODO: handle exception
-        } 
-
-        
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                try {
+                    System.out.println("Enter amount");
+                    amounts[0] = Double.parseDouble(scanner.nextLine());
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid amount. Try again.");
+                }
+            }
+        }
+        System.out.println("Amount accepted: " + amounts[0]);
+    
     }
 }
