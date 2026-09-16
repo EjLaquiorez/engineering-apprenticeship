79b8e2ac6ab8eb5db79c2106bdd42b534724593b
EjLaquiorez <ejlqrz@gmail.com>
2026-09-16 17:44:51 +0800

    projects: miniExercise -initial setup

---
+++ projects/phase-01-java-foundations/expense-tracker/miniExercise.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class miniExercise {
+    public static void main(String[] args){
+        String[] descriptions = new String[3];
+        double[] amounts = new double[3];
+        int expenseCount = 0;
+
+        try(Scanner scanner = new Scanner(System.in)){
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        } 
+
+        
+    }
+}
