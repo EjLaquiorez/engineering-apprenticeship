d586899471a4e2b1c4ebb87d73b92d8dde829087
EjLaquiorez <ejlqrz@gmail.com>
2026-09-16 21:47:18 +0800

    projects: miniExercise -String validation

--- projects/phase-01-java-foundations/expense-tracker/miniExercise.java
+++ projects/phase-01-java-foundations/expense-tracker/miniExercise.java
@@ -4,8 +4,17 @@ public class miniExercise {
         String[] descriptions = new String[3];
         double[] amounts = new double[3];
         int expenseCount = 0;
-
-        try (Scanner scanner = new Scanner(System.in)) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter description");
+        while (true) {
+            descriptions[0] = scanner.nextLine().trim();
+            if (!descriptions[0].isEmpty()) {
+                break;
+            }
+            System.out.println("Description cannot be empty. Try again.");
+        }
+        try {
             while (true) {
                 try {
                     System.out.println("Enter amount");
@@ -16,7 +25,8 @@ public class miniExercise {
                 }
             }
         }
-        System.out.println("Amount accepted: " + amounts[0]);
-    
+        expenseCount++;
+        System.out.println("Amount accepted: " + amounts[0]);        
+        System.out.println(expenseCount);
     }
 }
