473aa528779f66da0d130c122ec1b5b5b17c49ee
EjLaquiorez <ejlqrz@gmail.com>
2026-09-18 09:07:43 +0800

    projects: Student Management System -Practice file

---
+++ projects/phase-01-java-foundations/student-management-system/Lesson01Exercise.java
@@ -0,0 +1,12 @@
+public class Lesson01Exercise {
+	public static void main(String[] args) {
+
+        String[] names = new String[5];
+        int[] ages = new int[5];
+		double[] grades = new double[5];
+
+		int studentCount = 0;
+
+		System.out.println("Student Management System");
+	}
+}
