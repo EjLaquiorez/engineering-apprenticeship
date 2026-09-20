6715239ab431bf0bab57b545e7c330895e8ad1eb
EjLaquiorez <ejlqrz@gmail.com>
2026-09-20 18:58:00 +0800

    exercise(01-classes-and-objects): add Lesson 05 exercises

---
+++ exercises/phase-02-object-oriented-programming/01-classes-and-objects/Exercise05C.java
@@ -0,0 +1,14 @@
+public class Exercise05C {
+
+    static class Dog {
+        String name;
+    }
+
+    public static void main(String[] args) {
+
+        Dog dog = null;
+
+        System.out.println(dog);
+        System.out.println(dog.name);
+	}
+}
