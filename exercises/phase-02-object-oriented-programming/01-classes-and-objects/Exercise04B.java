c1a92b29b9c8f52af535e804d52ea2da17621130
EjLaquiorez <ejlqrz@gmail.com>
2026-09-20 13:05:16 +0800

    exercise(01-classes-and-objects): add Lesson 04 exercises

--- exercises/phase-02-object-oriented-programming/01-classes-and-objects/Exercise04B.java
+++ exercises/phase-02-object-oriented-programming/01-classes-and-objects/Exercise04B.java
@@ -22,5 +22,15 @@ public class Exercise04B {
         student2.grade = 92.5;
         student2.enrolled = true;
         
+        System.out.println("Name: " + student1.name);
+        System.out.println("Age: " + student1.age);
+        System.out.println("Grade: " + student1.grade);
+        System.out.println("Enrolled: " + student1.enrolled);
+
+        System.out.println("Name: " + student2.name);
+        System.out.println("Age: " + student2.age);
+        System.out.println("Grade: " + student2.grade);
+        System.out.println("Enrolled: " + student2.enrolled);
+        
     }
 }
