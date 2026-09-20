8f4875acfcbe0e2315641756716b80941b4d0b9d
EjLaquiorez <ejlqrz@gmail.com>
2026-09-20 10:17:22 +0800

    exercise(01-classes-and-objects): add Lesson 03 exercises

---
+++ exercises/phase-02-object-oriented-programming/01-classes-and-objects/Exercise03B.java
@@ -0,0 +1,12 @@
+public class Exercise03B {
+
+    static class Dog{
+        String color;
+        String breed;
+    }
+    public static void main(String[] args) {
+        Dog dog1 = new Dog();
+        Dog dog2 = new Dog();
+        Dog dog3 = new Dog();
+    }
+}
