76c2981b35a2aa36feb4048fda9c347b4aad81fd
EjLaquiorez <ejlqrz@gmail.com>
2026-09-20 10:38:39 +0800

    docs(01-classes-and-objects): add 01.3 what is an object lesson

---
+++ exercises/phase-02-object-oriented-programming/01-classes-and-objects/Exercise03C.java
@@ -0,0 +1,23 @@
+public class Exercise03C {
+
+    static class Dog{
+        String color;
+        String breed;
+    }
+
+    
+    public static void main(String[] args) {
+        Dog dog1 = new Dog();
+        Dog dog2 = new Dog();
+        Dog dog3 = new Dog();
+
+        dog1.color = "Brown";
+        dog1.breed = "Labrador";
+        dog2.color = "Black";
+        dog2.breed = "German Shepherd";
+        dog3.color = "White";
+        dog3.breed = "Poodle";
+
+        
+    }
+}
