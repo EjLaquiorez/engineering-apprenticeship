b5b4074576439b94a5e8c383d05a9f80fb61b233
EjLaquiorez <ejlqrz@gmail.com>
2026-09-21 17:47:51 +0800

    exercise(01-classes-and-objects): add Lesson 06 exercises

--- exercises/phase-02-object-oriented-programming/01-classes-and-objects/Exercise06D.java
+++ exercises/phase-02-object-oriented-programming/01-classes-and-objects/Exercise06D.java
@@ -1,21 +1,19 @@
 public class Exercise06D {
     public static void main(String[] args) {
         Person person1 = new Person();
-        person1.name = "Justine";
-        person1.age = 20;
-        
 
-        System.out.println("My name is " + person1.name + " and my age is " + person1.age);
+        person1.setInfo("Justine", 20);
+        System.out.println(person1.name + " " + person1.age);
+        
     }
 
-    static class Person{
+    static class Person {
         String name;
         int age;
 
-        void setInfo(String name, int age){
+        void setInfo(String name, int age) {
             this.name = name;
             this.age = age;
         }
     }
 }
-
