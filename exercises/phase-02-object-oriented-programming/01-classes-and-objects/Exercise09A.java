e53315ba3fe933859fc020f169076e395c819b0f
EjLaquiorez <ejlqrz@gmail.com>
2026-09-24 11:18:37 +0800

    exercise(01-classes-and-objects): add Lesson 09 exercises

--- exercises/phase-02-object-oriented-programming/01-classes-and-objects/Exercise09A.java
+++ exercises/phase-02-object-oriented-programming/01-classes-and-objects/Exercise09A.java
@@ -1,19 +1,38 @@
-class Student {
-    private String name;
-    private int age;
-    private double grade;
+public class Exercise09A{
+    static class Dog{
+        String name;
+        String color;
+        String breed;
 
-Student() {
-    this("Unknown", 0, 0.0);
-}
+        Dog(String name, String color, String breed){
+            this.name = name;
+            this.color = color;
+            this.breed = breed;
+        }
+        Dog(String name){
+            this(name, "Brown", "Labrador");
 
-Student(String name) {
-    this(name, 0, 0.0);
-}
+        }
+        void bark(){
+            System.out.println(name + ", says woof!");
+        }
 
-Student(String name, int age, double grade) {
-    this.name = name;
-    this.age = age;
-    this.grade = grade;
-}
+        void sit(){
+            System.out.println(name + ", is sitting.");
+        }
+
+        void stay(){
+            System.out.println(name + ", is staying.");
+        }
+        
+    }
+
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("brownie");
+
+        dog1.bark();
+        dog1.sit();
+        dog1.stay();
+        
+    }
 }
