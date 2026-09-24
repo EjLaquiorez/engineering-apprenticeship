69ec0da12376a03de161b1d6235ee08ef95d4e1f
EjLaquiorez <ejlqrz@gmail.com>
2026-09-24 16:27:34 +0800

    exercise(01-classes-and-objects): add Lesson 09 exercises

---
+++ exercises/phase-02-object-oriented-programming/01-classes-and-objects/Exercise09B.java
@@ -0,0 +1,18 @@
+class Car {
+    String brand;
+    String model;
+    int year;
+    String color;
+
+    void start() {
+        System.out.println("Car started.");
+    }
+
+    void stop() {
+        System.out.println("Car stopped.");
+    }
+
+    void displayInfo() {
+        System.out.println(brand + " " + model);
+    }
+}
